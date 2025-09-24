735db1b5f17c3428c6c6fa71749c205e156a1cf2
PaulanDev <132430937+PaulanDev@users.noreply.github.com>
2025-09-24 03:14:48 -0400

    Update script.js
    Added filtering when selection divs are clicked

--- script.js
+++ script.js
@@ -9,23 +9,55 @@ let selectedTypes = [];
 
 for (let i = 0; i < selectionBtns.length; i++) {
   selectionBtns[i].addEventListener("click", () => {
+    //Reset the display div
+    ADVDISPLAY.innerHTML = ``;
+
+    //Toggle div buttons on click
     selectionBtns[i].classList.toggle("sel-div-active");
-    selectedTiers = document.querySelectorAll(
-      ".sel-div.tier-sel-div.sel-div-active"
-    );
-    selectedTypes = document.querySelectorAll(
-      ".sel-div.type-sel-div.sel-div-active"
-    );
-    console.log(selectedTiers);
-    console.log(selectedTypes);
+
+    //Set selections arrays
+    selectedTiers = Array.from(
+      document.querySelectorAll(".sel-div.tier-sel-div.sel-div-active")
+    ).map((item) => item.id.slice(0, -3));
+
+    selectedTypes = Array.from(
+      document.querySelectorAll(".sel-div.type-sel-div.sel-div-active")
+    ).map((item) => item.id.slice(0, -3));
+
+    //Display cards
+    if (selectedTiers.length == 0 && selectedTypes.length == 0) {
+      for (let i = 0; i < adversaryArr.length; i++) {
+        ADVDISPLAY.innerHTML += createAdvCard(adversaryArr[i]);
+      }
+    } else if (selectedTypes.length > 0 && selectedTiers.length == 0) {
+      for (let i = 0; i < adversaryArr.length; i++) {
+        if (selectedTypes.includes(adversaryArr[i].type.toLowerCase())) {
+          ADVDISPLAY.innerHTML += createAdvCard(adversaryArr[i]);
+        }
+      }
+    } else if (selectedTiers.length > 0 && selectedTypes.length == 0) {
+      for (let i = 0; i < adversaryArr.length; i++) {
+        if (selectedTiers.includes("tier" + adversaryArr[i].tier)) {
+          ADVDISPLAY.innerHTML += createAdvCard(adversaryArr[i]);
+        }
+      }
+    } else {
+      for (let i = 0; i < adversaryArr.length; i++) {
+        if (
+          selectedTiers.includes("tier" + adversaryArr[i].tier) &&
+          selectedTypes.includes(adversaryArr[i].type.toLowerCase())
+        ) {
+          ADVDISPLAY.innerHTML += createAdvCard(adversaryArr[i]);
+        }
+      }
+    }
   });
 }
 
+//Create cards on load
 window.addEventListener("load", function () {
   //If there are no selected parameters for tier or type
-  if (selectedTiers.length == 0 && selectedTypes.length == 0) {
-    for (let i = 0; i < adversaryArr.length; i++) {
-      ADVDISPLAY.innerHTML += createAdvCard(adversaryArr[i]);
-    }
+  for (let i = 0; i < adversaryArr.length; i++) {
+    ADVDISPLAY.innerHTML += createAdvCard(adversaryArr[i]);
   }
 });
